f000c37aaa5da5976952103095d1fc1173a23c70
Gildo Alves <104049719+16b8f3@users.noreply.github.com>
2022-05-20 14:37:15 -0300

    A versão finalizada só que bem simples
    vamos melhorar ela, vamos sim

--- js/javaScript.js
+++ js/javaScript.js
@@ -1,1 +1,17 @@
-console.log('Oi meu chapa');
+let btn = document.querySelector('.btn');
+let popup = document.querySelector('.popup');
+let close = document.querySelector('.close');
+let confe = document.querySelector('#my-canvas');
+var confettiSettings = { target: 'my-canvas' };
+var confetti = new ConfettiGenerator(confettiSettings);
+confetti.render();
+
+btn.onclick = function() {
+	popup.classList.add('active');
+	confe.classList.add('active');
+}
+
+close.onclick = function() {
+	popup.classList.remove('active');
+	confe.classList.remove('active');
+}
